cdd3fc845f8ebd0f8bc9e66c41abbb276df304b0
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-04-28 13:03:10 +0300

    Show pokemons on mainpage

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,25 +1,56 @@
-from django.db import models  # noqa F401
+from django.db import models
 
-# your models here
+
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='pokemons', blank=True, null=True)
+    image = models.ImageField(
+        upload_to='pokemons',
+        blank=True, null=True
+    )
 
     def __str__(self):
-        return self.title    # noqa F405
+        return self.title
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    latitude = models.FloatField(verbose_name='Lat.', null=False)
-    longitude = models.FloatField(verbose_name='Lon.', null=False)
-    appeared_at = models.DateTimeField(verbose_name='Appeared at', blank=True, null=True)
-    disappeared_at = models.DateTimeField(verbose_name='Disappeared at', blank=True, null=True)
-    level = models.IntegerField(verbose_name='Level', blank=True, null=True)
-    helth = models.IntegerField(verbose_name='Health', blank=True, null=True)
-    strength = models.IntegerField(verbose_name='Strength', blank=True, null=True)
-    defence = models.IntegerField(verbose_name='Defence', blank=True, null=True)
-    stamina = models.IntegerField(verbose_name='Stamina', blank=True, null=True)
+    pokemon = models.ForeignKey(
+        Pokemon,
+        on_delete=models.CASCADE
+    )
+    lat = models.FloatField(
+        null=False, verbose_name='Lat.',
+    )
+    lon = models.FloatField(
+        null=False, verbose_name='Lon.',
+    )
+    appeared_at = models.DateTimeField(
+        blank=True, null=True,
+        verbose_name='Appeared at',
+    )
+    disappeared_at = models.DateTimeField(
+        blank=True, null=True,
+        verbose_name='Disappeared at',
+    )
+    level = models.IntegerField(
+        blank=True, null=True,
+        verbose_name='Level',
+    )
+    helth = models.IntegerField(
+        blank=True, null=True,
+        verbose_name='Health',
+    )
+    strength = models.IntegerField(
+        blank=True, null=True,
+        verbose_name='Strength',
+    )
+    defence = models.IntegerField(
+        blank=True, null=True,
+        verbose_name='Defence',
+    )
+    stamina = models.IntegerField(
+        blank=True, null=True,
+        verbose_name='Stamina',
+    )
 
     def __str__(self):
-        return self.latitude, self.longitude
+        return self.lat, self.lon
