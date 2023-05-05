b4d81df33988035ebf99048bfd0b1d56791cd266
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-05-05 09:45:02 +0300

    Fix show_all_pokemons

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -63,7 +63,7 @@ def show_all_pokemons(request):
         'mainpage.html',
         context={
             'map': folium_map._repr_html_(),
-            'pokemons': pokemons_without_duplicates,
+            'pokemons': pokemons_on_page,
         }
     )
 
