cf1a07c4e45db99b802de55064fcdf3d4dca293e
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-05-08 11:16:28 +0300

    Fixed folium_map (views.py)

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -24,13 +24,15 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-    folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    pokemons_on_page = []
     current_time = localtime()
     pokemon_entities = PokemonEntity.objects.filter(
         appeared_at__lte=current_time,
         disappeared_at__gt=current_time,
     )
+
+    folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
+
+    pokemons_on_page = []
     for pokemon_entity in pokemon_entities:
         pokemons_on_page.append(
             {
